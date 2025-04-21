5dc1f212f9c7d6ce14764027be5f1dd882cc978b
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2025-04-20 19:26:20 -0700

    added a feed forward layers in the network

--- gpt.py
+++ gpt.py
@@ -117,6 +117,17 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linear activation"""
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU()
+        )
+    def forward(self, x):
+        return self.net(x)
+
 # Creating Model
 class BigramLanguageModel(nn.Module):
     def __init__(self):
@@ -125,6 +136,7 @@ class BigramLanguageModel(nn.Module):
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         # self.sa_head = Head(n_embd)
         self.sa_heads = MultiHeadAttention(4, n_embd//4)                        # 4 heads of 8-dimentional self-attention
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
